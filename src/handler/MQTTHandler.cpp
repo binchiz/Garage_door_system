a9f71bce90ccf8751181ab21a92a12e2ed5d65c1
zhiyo <1099207684@qq.com>
2025-03-04 20:44:04 +0200

    mqtt part

--- src/handler/MQTTHandler.cpp
+++ src/handler/MQTTHandler.cpp
@@ -3,3 +3,71 @@
 //
 
 #include "MQTTHandler.h"
+#include <iostream>
+
+MQTTHandler_t* MQTTHandler_t::currentHandler = nullptr;  // 在cpp文件中定义
+
+//
+// MQTTHandler_t::MQTTHandler_t(DoorController_t &controller, GarageDoorSystem &system,
+//                              const std::string &ssid, const std::string &pw,
+//                              const string &hostname, int port, const string &clientID)
+//     : controller(controller), system(system), Mqtt_tool(ssid, pw, hostname, port, clientID) {
+//     connect_MQTT();
+//     subscribe_MQTT(COMMAND_TOPIC, MQTT::QOS1);
+//     subscribe_MQTT(STATUS_TOPIC, MQTT::QOS1);
+//     subscribe_MQTT(ERROR_TOPIC, MQTT::QOS1);
+//     currentHandler = this;
+// }
+
+
+MQTTHandler_t::MQTTHandler_t(
+                             const std::string &ssid, const std::string &pw,
+                             const string &hostname, int port, const string &clientID)
+    : Mqtt_tool(ssid, pw, hostname, port, clientID) {
+    connect_MQTT();
+    subscribe_MQTT(COMMAND_TOPIC, MQTT::QOS1);
+    subscribe_MQTT(STATUS_TOPIC, MQTT::QOS1);
+    subscribe_MQTT(ERROR_TOPIC, MQTT::QOS1);
+    currentHandler = this;
+}
+
+// DoorController_t& MQTTHandler_t::getController() {
+//     return controller;
+// }
+//
+// GarageDoorSystem& MQTTHandler_t::getSystem() {
+//     return system;
+// }
+
+void MQTTHandler_t::messageHandler(MQTT::MessageData &md) {
+    if (currentHandler) {
+        currentHandler->processMessage(md);
+    }
+}
+
+void MQTTHandler_t::processMessage(MQTT::MessageData &md) {
+    MQTT::Message &message = md.message;
+    std::vector<char> temp_buffer(message.payloadlen + 1, '\0');
+    memcpy(temp_buffer.data(), message.payload, message.payloadlen);
+    std::string mess = temp_buffer.data();
+    std::cout << "Message received [length: " << message.payloadlen
+            << "]: '" << temp_buffer.data() << "'" << std::endl;
+
+    // Status_t status = controller.getDoorStatus();
+    // if(status.calibState == UNCALIBRATED) {
+    //     system.addCommand(CALIB);
+    //     return;
+    // }
+    // system.addCommand(CALIB);
+}
+
+
+
+// MQTTHandler_t::MQTTHandler_t(const std::string &ssid, const std::string &pw, const string &hostname, int port,
+//     const string &clientID) : Mqtt_tool(ssid, pw, hostname, port, clientID) {
+//     connect_MQTT();
+//     subscribe_MQTT(COMMAND_TOPIC, MQTT::QOS1);
+//     subscribe_MQTT(STATUS_TOPIC, MQTT::QOS1);
+//     subscribe_MQTT(ERROR_TOPIC, MQTT::QOS1);
+// }
+
---
+++ src/main.cpp
@@ -0,0 +1,49 @@
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include "pico/stdlib.h"
+#include "pico/time.h"
+#include "hardware/timer.h"
+
+#include "MQTTClient.h"
+#include "./handler/lib/Mqtt_tool.h"
+#include "Countdown.h"
+#include "IPStack.h"
+#include "handler/MQTTHandler.h"
+
+int main() {
+    stdio_init_all();
+
+    // 创建MQTT处理器
+    MQTTHandler_t mqtt_handler{
+        // Controller,          // 门控制器
+        // System,              // 门系统
+        "Redmi_138D",     // WiFi SSID
+        "zzyzmy20272025888", // WiFi 密码
+        "192.168.31.12",    // MQTT 服务器地址
+        2883,               // MQTT 服务器端口
+        "GarageDoor-Client" // 客户端ID
+    };
+
+    string message = "garage/door/status, hello,world";
+    mqtt_handler.publish_MQTT(MQTT::QOS1, "garage/door/status", message.data(), message.size());
+
+
+    while (true) {
+        //assume 50s
+        int count = 10;
+        int rc = mqtt_handler.yield_MQTT(2000);
+        if (rc != 0) {
+            printf("rc from yield is %d\n", rc);
+            printf("Reconnect to MQTT broker \n");
+            mqtt_handler.connect_MQTT();
+            mqtt_handler.subscribe_MQTT("garage/door/command", MQTT::QOS1);
+        }
+        while (count > 0) {
+            printf("count: %d ", count);
+            count--;
+            sleep_ms(5000);
+        }
+        printf("\n");
+    }
+}
