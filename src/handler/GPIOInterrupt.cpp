ee8d5ae34b9445e91d887a9e825637f9b47570ed
Sheng Tai <alxt9@outlook.com>
2025-03-08 11:46:19 +0200

    added global GPIO interrupt handler

---
+++ src/handler/GPIOInterrupt.cpp
@@ -0,0 +1,51 @@
+//
+// Created by Sheng Tai on 07/03/2025.
+//
+
+#include "GPIOInterrupt.h"
+#include <iostream>
+
+GPIOInterrupt::IrqCallback GPIOInterrupt::callbacks[MAX_GPIO_PINS] = {nullptr};
+uint32_t GPIOInterrupt::callbacksEvents[MAX_GPIO_PINS] = {0};
+void *GPIOInterrupt::contexts[MAX_GPIO_PINS] = {nullptr};
+
+void GPIOInterrupt::init() {
+    gpio_set_irq_callback(globalGPIOCallback);
+    irq_set_enabled(IO_IRQ_BANK0, true);
+}
+
+void GPIOInterrupt::registerCallback(const uint pin, const uint32_t events, const IrqCallback callback,
+                                     void *context) {
+    assert(pin < MAX_GPIO_PINS); // GPIO number error
+    assert(callbacks[pin] == nullptr); // callback already registered
+    callbacks[pin] = callback;
+    contexts[pin] = context;
+    callbacksEvents[pin] = events;
+}
+
+void GPIOInterrupt::unregisterCallback(const uint pin) {
+    assert(pin < MAX_GPIO_PINS); // GPIO number error
+    assert(callbacks[pin] != nullptr); // callback not registered
+    gpio_set_irq_enabled(pin, callbacksEvents[pin], false);
+    callbacks[pin] = nullptr;
+    contexts[pin] = nullptr;
+    callbacksEvents[pin] = 0;
+}
+
+void GPIOInterrupt::enable(const uint pin) {
+    assert(pin < MAX_GPIO_PINS); // GPIO number error
+    assert(callbacks[pin] != nullptr); // callback not registered
+    gpio_set_irq_enabled(pin, callbacksEvents[pin], true);
+}
+
+void GPIOInterrupt::disable(const uint pin) {
+    assert(pin < MAX_GPIO_PINS); // GPIO number error
+    assert(callbacks[pin] != nullptr); // callback not registered
+    gpio_set_irq_enabled(pin, callbacksEvents[pin], false);
+}
+
+void GPIOInterrupt::globalGPIOCallback(const uint gpio, const uint32_t events) {
+    assert(gpio < MAX_GPIO_PINS); // GPIO number error
+    assert(callbacks[gpio] != nullptr); // callback not registered
+    callbacks[gpio](contexts[gpio], events);
+}
---
+++ src/handler/GPIOInterrupt.h
@@ -0,0 +1,28 @@
+//
+// Created by Sheng Tai on 07/03/2025.
+//
+
+#ifndef GARAGE_DOOR_SYSTEM_GPIOINTERRUPT_H
+#define GARAGE_DOOR_SYSTEM_GPIOINTERRUPT_H
+
+#include <pico/stdlib.h>
+
+class GPIOInterrupt {
+public:
+    using IrqCallback = void (*)(void *context, uint32_t events);
+    static void init();
+    static void registerCallback(uint pin, uint32_t events, IrqCallback callback, void *context);
+    static void unregisterCallback(uint pin);
+    static void enable(uint pin);
+    static void disable(uint pin);
+
+private:
+    static constexpr int MAX_GPIO_PINS = 30;
+    static IrqCallback callbacks[MAX_GPIO_PINS];
+    static uint32_t callbacksEvents[MAX_GPIO_PINS];
+    static void *contexts[MAX_GPIO_PINS];
+    static void globalGPIOCallback(uint gpio, uint32_t events);
+
+};
+
+#endif // GARAGE_DOOR_SYSTEM_GPIOINTERRUPT_H
