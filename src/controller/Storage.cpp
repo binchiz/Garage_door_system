900553c764b87db59a2555be7cfd7a484e0fccb0
Sheng Tai <alxt9@outlook.com>
2025-03-08 21:34:34 +0200

    implemented storage

--- src/controller/Storage.cpp
+++ src/controller/Storage.cpp
@@ -1,11 +1,100 @@
 #include "Storage.h"
 
-Storage_t::Storage_t(const EEPROM_t &eeprom) : eeprom(eeprom) {
+
+Storage_t::Storage_t(const EEPROM_t &eeprom, Status_t &status)
+        : eeprom(eeprom), status(status) {
+
 }
 
+
+// saves
 void Storage_t::saveCalib() const {
-    // need to pass data here
-    if (!eeprom.writeByteWithChecksum(calibAddr, calibChecksumAddr, 0)) {
-        // failed save data
+    auto data = static_cast<uint8_t>(status.calibState);
+    if (!eeprom.writeByteWithChecksum(ADDR_CALIB, ADDR_CALIB_CSUM, data)) {
+        // failed save data, set error and blink led as well?
     }
+}
+
+void Storage_t::savePos() const {
+    auto dataL = static_cast<uint8_t>(status.currentPosition & 0xFF);
+    auto dataH = static_cast<uint8_t>(status.currentPosition >> 8);
+    if (!eeprom.writeByteWithChecksum(ADDR_POS_L, ADDR_POS_L_CSUM, dataL)) {
+        // failed save data, set error and blink led as well?
+    }
+    if (!eeprom.writeByteWithChecksum(ADDR_POS_H, ADDR_POS_H_CSUM, dataH)) {
+        // failed save data, set error and blink led as well?
+    }
+}
+
+void Storage_t::saveTotalSteps() const {
+    auto dataL = static_cast<uint8_t>(status.totalSteps & 0xFF);
+    auto dataH = static_cast<uint8_t>(status.totalSteps >> 8);
+    if (!eeprom.writeByteWithChecksum(ADDR_TOTAL_STEPS_L, ADDR_TOTAL_STEPS_L_CSUM, dataL)) {
+        // failed save data, set error and blink led as well?
+    }
+    if (!eeprom.writeByteWithChecksum(ADDR_TOTAL_STEPS_H, ADDR_TOTAL_STEPS_H_CSUM, dataH)) {
+        // failed save data, set error and blink led as well?
+    }
+}
+
+void Storage_t::saveState() const {
+    auto data = static_cast<uint8_t>(status.doorState);
+    if (!eeprom.writeByteWithChecksum(ADDR_STATE, ADDR_STATE_CSUM, data)) {
+        // failed save data, set error and blink led as well?
+    }
+}
+
+void Storage_t::saveError() const {
+    auto data = static_cast<uint8_t>(status.errorState);
+    if (!eeprom.writeByteWithChecksum(ADDR_ERROR, ADDR_ERROR_CSUM, data)) {
+        // failed save data, set error and blink led as well?
+    }
+}
+
+
+// loads
+void Storage_t::loadCalib() {
+    uint8_t data;
+    if (!eeprom.readByteWithChecksum(ADDR_CALIB, ADDR_CALIB_CSUM, &data)) {
+        // failed to read data, set error and blink led as well?
+    }
+    status.calibState = static_cast<calibState_t>(data);
+}
+
+void Storage_t::loadPos() {
+    uint8_t dataL, dataH;
+    if (!eeprom.readByteWithChecksum(ADDR_POS_L, ADDR_POS_L_CSUM, &dataL)) {
+        // failed to read data, set error and blink led as well?
+    }
+    if (!eeprom.readByteWithChecksum(ADDR_POS_H, ADDR_POS_H_CSUM, &dataH)) {
+        // failed to read data, set error and blink led as well?
+    }
+    status.currentPosition = static_cast<int>(dataL) | (static_cast<int>(dataH) << 8);
+}
+
+void Storage_t::loadTotalSteps() {
+    uint8_t dataL, dataH;
+    if (!eeprom.readByteWithChecksum(ADDR_TOTAL_STEPS_L, ADDR_TOTAL_STEPS_L_CSUM, &dataL)) {
+        // failed to read data, set error and blink led as well?
+    }
+    if (!eeprom.readByteWithChecksum(ADDR_TOTAL_STEPS_H, ADDR_TOTAL_STEPS_H_CSUM, &dataH)) {
+        // failed to read data, set error and blink led as well?
+    }
+    status.totalSteps = static_cast<int>(dataL) | (static_cast<int>(dataH) << 8);
+}
+
+void Storage_t::loadState() {
+    uint8_t data;
+    if (!eeprom.readByteWithChecksum(ADDR_STATE, ADDR_STATE_CSUM, &data)) {
+        // failed to read data, set error and blink led as well?
+    }
+    status.doorState = static_cast<GarageDoor::doorState_t>(data);
+}
+
+void Storage_t::loadError() {
+    uint8_t data;
+    if (!eeprom.readByteWithChecksum(ADDR_ERROR, ADDR_ERROR_CSUM, &data)) {
+        // failed to read data, set error and blink led as well?
+    }
+    status.errorState = static_cast<errorState_t>(data);
 }
--- src/controller/Storage.h
+++ src/controller/Storage.h
@@ -9,33 +9,47 @@
 #ifndef STORAGE_H
 #define STORAGE_H
 #include "hardware/EEPROM.h"
+#include "types.h"
 
 class Storage_t {
     public:
-        explicit Storage_t(const EEPROM_t &eeprom);
+        explicit Storage_t(const EEPROM_t &eeprom, Status_t &status);
         ~Storage_t() = default;
         void saveCalib() const;
-        void savePos();
-        void saveState();
-        void saveError();
-        void saveTotalSteps();
+        void savePos() const;
+        void saveTotalSteps() const;
+        void saveState() const;
+        void saveError() const;
 
         void loadCalib();
         void loadPos();
+        void loadTotalSteps();
         void loadState();
         void loadError();
-        void loadTotalSteps();
 
     private:
         const EEPROM_t& eeprom;
-        const uint16_t calibAddr = 0x00;
-        const uint16_t calibChecksumAddr = calibAddr + 1;
-        const uint16_t posAddr = 0x02;
-        const uint16_t posChecksumAddr = posAddr + 1;
-        const uint16_t stateAddr = 0x04;
-        const uint16_t stateChecksumAddr = stateAddr + 1;
-        const uint16_t errorAddr = 0x06;
-        const uint16_t errorChecksumAddr = errorAddr + 1;
+        Status_t &status;
+        const uint16_t ADDR_CALIB = 0x00;
+        const uint16_t ADDR_CALIB_CSUM = ADDR_CALIB + 1;
+
+        const uint16_t ADDR_POS_L = 0x02;
+        const uint16_t ADDR_POS_L_CSUM = ADDR_POS_L + 1;
+
+        const uint16_t ADDR_POS_H = 0x04;
+        const uint16_t ADDR_POS_H_CSUM = ADDR_POS_H + 1;
+
+        const uint16_t ADDR_TOTAL_STEPS_L = 0x06;
+        const uint16_t ADDR_TOTAL_STEPS_L_CSUM = ADDR_TOTAL_STEPS_L + 1;
+
+        const uint16_t ADDR_TOTAL_STEPS_H = 0x08;
+        const uint16_t ADDR_TOTAL_STEPS_H_CSUM = ADDR_TOTAL_STEPS_H + 1;
+
+        const uint16_t ADDR_STATE = 0x010;
+        const uint16_t ADDR_STATE_CSUM = ADDR_STATE + 1;
+
+        const uint16_t ADDR_ERROR = 0x012;
+        const uint16_t ADDR_ERROR_CSUM = ADDR_ERROR + 1;
 };
 
 #endif //STORAGE_H
